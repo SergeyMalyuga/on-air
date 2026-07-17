4b47c88bf25e16da54adca27fcb951bd894d4ceb
Sergey Malyuga <sergey@malyuga.ru>
2026-07-17 15:04:03 +0300

    feat(header): создать компонент

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {AppRoute} from '../../../core/constants/const';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  protected readonly AppRoute = AppRoute;
+}
