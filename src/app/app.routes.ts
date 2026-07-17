898cef960a648be003c97dc41516b3962b3d1cd7
Sergey Malyuga <sergey@malyuga.ru>
2026-07-17 15:08:34 +0300

    feat(app.routes): настроить маршрут для главной страницы

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {AppRoute} from './core/constants/const';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: AppRoute.MAIN,
+    title: 'MainComponent page',
+    loadComponent: () => import('./pages/main/main.component').then(m => m.MainComponent)
+  }
+];
