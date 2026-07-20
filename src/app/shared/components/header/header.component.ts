afc786257794b928efc12a8bf1f0ed9c6a97d319
Sergey Malyuga <sergey@malyuga.ru>
2026-07-20 15:04:48 +0300

    feat(header): адаптировать меню навигации при переходе с мобильных устройств

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,7 +1,8 @@
-import {Component, signal} from '@angular/core';
+import {Component, effect, inject, signal} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {AppRoute} from '../../../core/constants/const';
 import {NgClass} from '@angular/common';
+import {ScreenService} from '../../../core/services/screen.service';
 
 @Component({
   selector: 'app-header',
@@ -13,9 +14,20 @@ import {NgClass} from '@angular/common';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  private screenService = inject(ScreenService);
+
   protected readonly AppRoute = AppRoute;
 
   public isNavMenuOpen = signal<boolean>(false);
+
+  constructor() {
+    effect(() => {
+      const isMobile = this.screenService.isMobile();
+      if (!isMobile) {
+        this.isNavMenuOpen.set(false);
+      }
+    });
+  }
 
   public toggleNavMenu() {
     this.isNavMenuOpen.set(!this.isNavMenuOpen());
