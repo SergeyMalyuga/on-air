9e6731b1cc59925f4417d2bc4ca46813af42b143
Sergey Malyuga <sergey@malyuga.ru>
2026-07-20 14:26:14 +0300

    feat(header): адаптировать меню навигации под мобильные устройства

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {AppRoute} from '../../../core/constants/const';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   imports: [
-    RouterLink
+    RouterLink,
+    NgClass
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   protected readonly AppRoute = AppRoute;
+
+  public isNavMenuOpen = signal<boolean>(false);
+
+  public toggleNavMenu() {
+    this.isNavMenuOpen.set(!this.isNavMenuOpen());
+  }
 }
