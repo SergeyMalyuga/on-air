9d1fbc82e47f6d55744547458228d5b30b5dd5d0
Sergey Malyuga <sergey@malyuga.ru>
2026-07-21 15:22:14 +0300

    feat(home-page): добавить секцию hero
    - Содержит слайдер с превью фильмов

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from '../../shared/components/header/header.component';
+import {SwiperComponent} from '../../shared/components/swiper/swiper.component';
+import {MAIN_HERO_SWIPER_CONFIG} from '../../shared/components/swiper/swiper.config';
 
 @Component({
   selector: 'app-main',
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    SwiperComponent
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
-export class MainComponent {}
+export class MainComponent {
+  protected readonly MAIN_HERO_SWIPER_CONFIG = MAIN_HERO_SWIPER_CONFIG;
+}
