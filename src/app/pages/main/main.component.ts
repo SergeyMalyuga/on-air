0faf2230a2457a32595fa2df488b364ff575f46f
Sergey Malyuga <sergey@malyuga.ru>
2026-07-23 14:22:02 +0300

    feat(home-page): добавить секцию с рекомендованным контентом

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from '../../shared/components/header/header.component';
 import {SwiperComponent} from '../../shared/components/swiper/swiper.component';
 import {MAIN_HERO_SWIPER_CONFIG} from '../../shared/components/swiper/swiper.config';
+import {TopChannelsComponent} from './components/top-channels/top-channels.component';
 
 @Component({
   selector: 'app-main',
   imports: [
     HeaderComponent,
-    SwiperComponent
+    SwiperComponent,
+    TopChannelsComponent
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
