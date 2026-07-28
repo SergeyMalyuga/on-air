2129db50982f2a2fb8bcb93eb3311f28f15416cd
Sergey Malyuga <sergey@malyuga.ru>
2026-07-28 08:23:32 +0300

    feat(home-page): добавить блоки с новыми и популярными фильмами

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from '../../shared/components/header/header.component';
 import {SwiperComponent} from '../../shared/components/swiper/swiper.component';
 import {MAIN_HERO_SWIPER_CONFIG} from '../../shared/components/swiper/swiper.config';
-import {TopChannelsComponent} from './components/top-channels/top-channels.component';
+import {RecommendedComponent} from './components/top-channels/recommended.component';
 
 @Component({
   selector: 'app-main',
   imports: [
     HeaderComponent,
     SwiperComponent,
-    TopChannelsComponent
+    RecommendedComponent
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
