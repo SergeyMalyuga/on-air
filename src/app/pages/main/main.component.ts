24cc3dfdf434af1c99fd999f16d047ad4fc0e4db
Sergey Malyuga <sergey@malyuga.ru>
2026-07-28 09:38:30 +0300

    feat(home-page): добавить footer

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,15 +2,12 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from '../../shared/components/header/header.component';
 import {SwiperComponent} from '../../shared/components/swiper/swiper.component';
 import {MAIN_HERO_SWIPER_CONFIG} from '../../shared/components/swiper/swiper.config';
-import {RecommendedComponent} from './components/top-channels/recommended.component';
+import {RecommendedComponent} from './components/recommended/recommended.component';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-main',
-  imports: [
-    HeaderComponent,
-    SwiperComponent,
-    RecommendedComponent
-  ],
+  imports: [HeaderComponent, SwiperComponent, RecommendedComponent, FooterComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
